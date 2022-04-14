4a48bb054960e8c2a2d089df49bf23f459181755
IN-MP-003 <IN-MP-003@192.168.1.76>
2022-04-14 09:24:42 +0200

    edit servelet doPost

--- project-jee-tp/src/main/java/fr/formation/inti/controller/IndexController.java
+++ project-jee-tp/src/main/java/fr/formation/inti/controller/IndexController.java
@@ -1,6 +1,8 @@
 package fr.formation.inti.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,27 +16,21 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public IndexController() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
 		doGet(request, response);
 	}
 
